84e8e2c1dd891d01b51a12913c37edaa7a1a46ed
Dmitry <dmitrytbp@gmail.com>
2021-02-04 13:28:50 +0500

    Update Modal.jsx

--- client/src/components/FilesPage/Modal/Modal.jsx
+++ client/src/components/FilesPage/Modal/Modal.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import s from './Modal.module.css';
+import { useDispatch } from 'react-redux';
+import { toogleModal } from '../../../redux/filesPage-reducer';
+import style from './Modal.module.scss';
 
 function Modal(props) {
+  const { id, name, text } = props;
+  const dispatch = useDispatch();
+
   return (
-    <div className={s.container}>
-      <div className={s.modal}>
-        {props.name}
-        sdsd
-        <button onClick={props.closeModal}>Закрыть</button>
+    <div className={style.modal}>
+      <div className={style.modal__body}>
+        <h2 className={style.modal__title}> {name} </h2>
+        <form className={style.modal__form}>
+          <textarea
+            type='text'
+            value={text}
+            className={style.modal__textarea}
+          ></textarea>
+
+          <div className={style.modal__btns}>
+            <button className='btn' type='submit'>
+              Сохранить
+            </button>
+            <button
+              type='button'
+              className='btn'
+              onClick={() => dispatch(toogleModal(null))}
+            >
+              Закрыть
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
