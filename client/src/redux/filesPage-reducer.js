fdafe57e6ff2fc4698e698ea7a47cd711cda9a3f
Dmitry <dmitrytbp@gmail.com>
2021-02-04 13:28:19 +0500

    Update filesPage-reducer.js

--- client/src/redux/filesPage-reducer.js
+++ client/src/redux/filesPage-reducer.js
@@ -1,13 +1,28 @@
+const GET_FILES = 'GET_FILES';
+const MODAL = 'MODAL';
+
 let initialState = {
   files: [],
+  modal: {
+    currentItemId: null,
+    isOpen: false,
+  },
 };
 
 export const filesPageReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'GET_FILES':
+    case GET_FILES:
       return {
         ...state,
         files: action.files,
+      };
+    case MODAL:
+      return {
+        ...state,
+        modal: {
+          currentItemId: action.id,
+          isOpen: !state.modal.isOpen,
+        },
       };
     default:
       return { ...state };
@@ -15,6 +30,11 @@ export const filesPageReducer = (state = initialState, action) => {
 };
 
 export const setFiles = (files) => ({
-  type: 'GET_FILES',
-  files: files,
+  type: GET_FILES,
+  files,
+});
+
+export const toogleModal = (id) => ({
+  type: MODAL,
+  id,
 });
