a9ddd7fb56eee44932fb000c173e489bda5cf0ca
Dmitry <dmitrytbp@gmail.com>
2021-02-04 13:28:55 +0500

    Update FilesPage.jsx

--- client/src/components/FilesPage/FilesPage.jsx
+++ client/src/components/FilesPage/FilesPage.jsx
@@ -1,12 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import File from './File/File';
 import { setFiles } from '../../redux/filesPage-reducer';
+import Modal from './Modal/Modal';
 
 function FilesPage() {
   const dispatch = useDispatch();
   const files = useSelector((state) => state.filesPage.files);
+  const modal = useSelector((state) => state.filesPage.modal);
+
+  const [fileText, setFileText] = useState('');
+  let handleDownload = async (fileName) => {
+    const text = await axios
+      .post(
+        'http://localhost:5000/uploads',
+        { fileName },
+        { responseType: 'text' }
+      )
+      .then((response) => {
+        console.log(response);
+        return response.data;
+      });
+    setFileText(text);
+  };
+
   useEffect(() => {
     axios
       .get('http://localhost:5000/files')
@@ -25,10 +43,29 @@ function FilesPage() {
 
         <tbody className='files__list'>
           {files.map((file) => {
-            return <File id={file._id} name={file.name} key={file._id} />;
+            return (
+              <File
+                id={file._id}
+                name={file.name}
+                key={file._id}
+                handleDownload={handleDownload}
+              />
+            );
           })}
         </tbody>
       </table>
+      {files.map((file) => {
+        if (modal.isOpen && modal.currentItemId === file._id) {
+          return (
+            <Modal
+              id={file._id}
+              name={file.name}
+              text={fileText}
+              key={file._id}
+            />
+          );
+        }
+      })}
     </div>
   );
 }
