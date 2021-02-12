baa6bb273f4889f781be75026a87f5ac073cc680
Dmitry <dmitrytbp@gmail.com>
2021-02-12 20:08:50 +0500

    Create Registration.jsx

---
+++ client/src/components/authorization/Registration.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Registration() {
+  return (
+    <div className='auth-container'>
+      <form className='form'>
+        <div className='form__field'>
+          <label className='form__label' htmlFor='emailInput'>
+            Email
+          </label>
+          <input
+            id='emailInput'
+            type='text'
+            name='email'
+            className='input'
+          ></input>
+        </div>
+
+        <div className='form__field'>
+          <label className='form__label' htmlFor='passwordInput'>
+            Password
+          </label>
+          <input
+            id='passwordInput'
+            type='text'
+            name='password'
+            className='input'
+          ></input>
+        </div>
+
+        <button className='form__btn btn'>Зарегистрироваться</button>
+      </form>
+    </div>
+  );
+}
+
+export default Registration;
