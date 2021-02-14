68ef5d6e81255b004576bbce3eddd36f3264ba9e
Dmitry <dmitrytbp@gmail.com>
2021-02-14 15:35:42 +0500

    Refactoring

--- client/src/components/authorization/Login.jsx
+++ client/src/components/authorization/Login.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Login() {
   return (
     <div className='auth-container'>
       <form className='form'>
         <div className='form__field'>
-          <label className='form__label' htmlFor='emailInput'>
-            Email
-          </label>
           <input
-            id='emailInput'
             type='text'
             name='email'
-            className='input'
-          ></input>
+            className='form__input'
+            placeholder='email'
+          />
+          <span className='form__input-focus'></span>
+          <span className='form__input-icon'>
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
         </div>
 
         <div className='form__field'>
-          <label className='form__label' htmlFor='passwordInput'>
-            Password
-          </label>
           <input
-            id='passwordInput'
             type='text'
             name='password'
-            className='input'
-          ></input>
+            className='form__input'
+            placeholder='password'
+          />
+          <span className='form__input-focus'></span>
+          <span className='form__input-icon'>
+            <FontAwesomeIcon icon={faLock} />
+          </span>
         </div>
 
-        <button className='form__btn btn btn--login'>Войти</button>
+        <button className='form__btn btn btn--login'>Sign in</button>
       </form>
     </div>
   );
