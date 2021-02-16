d88fd897cadb7a85127dd2a4571c55c8d9cf585b
Dmitry <dmitrytbp@gmail.com>
2021-02-16 12:06:50 +0500

    Added express-validator

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const controller = require('../controllers/auth');
 const router = express.Router();
+const { check } = require('express-validator');
+const controller = require('../controllers/auth');
 
 router.post('/login', controller.login);
-router.post('/register', controller.register);
+router.post(
+  '/register',
+  [
+    check('email', 'Uncorrect email').isEmail(),
+    check('password', 'Password must be longer than 4 symbols').isLength({
+      min: 5,
+    }),
+  ],
+  controller.register
+);
 
 module.exports = router;
