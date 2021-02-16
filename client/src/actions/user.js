1598c2d13b0848450849844870ea4ed6d39572f4
Dmitry <dmitrytbp@gmail.com>
2021-02-16 12:08:01 +0500

    Create user.js

---
+++ client/src/actions/user.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const registration = async (email, password) => {
+  try {
+    const response = await axios.post(
+      'http://localhost:5000/api/auth/register',
+      {
+        email,
+        password,
+      }
+    );
+    alert(response.data.message);
+  } catch (error) {
+    console.log(error);
+  }
+};
