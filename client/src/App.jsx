2be3c2ccd213a3a9d40721928ce048c037dae282
Dmitry <dmitrytbp@gmail.com>
2021-02-28 17:45:18 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,20 @@ import Header from './components/Header/Header';
 import { Route, Redirect, BrowserRouter, Switch } from 'react-router-dom';
 import Login from './components/authorization/Login';
 import Registration from './components/authorization/Registration';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isAuth = useSelector((state) => state.userReducer.isAuth);
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Header />
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Registration} />
-          <Redirect from="/" to="/login" />
-        </Switch>
+        {!isAuth && (
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Registration} />
+          </Switch>
+        )}
       </div>
     </BrowserRouter>
   );
