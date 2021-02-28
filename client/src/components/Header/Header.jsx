32667d0f82cb8c9ecc86b5432f7427ee7e6de12a
Dmitry <dmitrytbp@gmail.com>
2021-02-28 17:45:09 +0500

    Added logout

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { logout } from '../../redux/userReducer';
 import styles from './Header.module.scss';
 
 function Header() {
+  const isAuth = useSelector((state) => state.userReducer.isAuth);
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.header}>
       <div className={styles.btns}>
-        <NavLink
-          to="/login"
-          activeClassName={styles.active}
-          className={styles.link}
-        >
-          <span>Sign in</span>
-        </NavLink>
-        <NavLink
-          to="/register"
-          activeClassName={styles.active}
-          className={styles.link}
-        >
-          <span>Sign up</span>
-        </NavLink>
+        {!isAuth && (
+          <NavLink
+            to='/login'
+            activeClassName={styles.active}
+            className={styles.link}
+          >
+            <span>Sign in</span>
+          </NavLink>
+        )}
+
+        {!isAuth && (
+          <NavLink
+            to='/register'
+            activeClassName={styles.active}
+            className={styles.link}
+          >
+            <span>Sign up</span>
+          </NavLink>
+        )}
+
+        {isAuth && (
+          <button
+            onClick={() => dispatch(logout())}
+            type='button'
+            className={styles.link}
+          >
+            <span>Log out</span>
+          </button>
+        )}
       </div>
     </div>
   );
