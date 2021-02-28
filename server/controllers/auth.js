35a573c8df5546044d9fbae7f8e41a6e6e83fd37
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-02-28 13:18:14 +0500

    Update

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -13,7 +13,7 @@ module.exports.login = async (req, res) => {
       const token = jwt.sign(
         {
           email: user.email,
-          userId: user._id,
+          userId: user._id
         },
         config.get('secretKey'),
         { expiresIn: 60 * 60 }
@@ -21,17 +21,19 @@ module.exports.login = async (req, res) => {
 
       res.status(200).json({
         token: `Bearer ${token}`,
-        email: user.email,
-        id: user._id,
+        user: {
+          email: user.email,
+          id: user._id
+        }
       });
     } else {
       res.status(401).json({
-        message: 'Invalid password',
+        message: 'Invalid password'
       });
     }
   } else {
     res.status(404).json({
-      message: 'User is not found',
+      message: 'User is not found'
     });
   }
 };
@@ -40,8 +42,8 @@ module.exports.register = async (req, res) => {
   const candidate = await User.findOne({ email: req.body.email });
 
   if (candidate) {
-    res.send({
-      message: 'Email already exist',
+    res.status(409).json({
+      message: `User with email ${req.body.email} already exist`
     });
   } else {
     const salt = bcrypt.genSaltSync(10);
@@ -49,16 +51,18 @@ module.exports.register = async (req, res) => {
 
     const user = new User({
       email: req.body.email,
-      password: bcrypt.hashSync(password, salt),
+      password: bcrypt.hashSync(password, salt)
     });
 
     try {
       await user.save();
-      res.status(201).json(user);
+      res.status(201).json({
+        message: 'User created'
+      });
     } catch (error) {
       res.send({
         success: false,
-        message: error.message ? error.message : error,
+        message: error.message ? error.message : error
       });
     }
   }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { check } = require('express-validator');
 const controller = require('../controllers/auth');
 
 router.post('/login', controller.login);
-router.post(
-  '/register',
-  [
-    check('email', 'Uncorrect email').isEmail(),
-    check('password', 'Password must be longer than 4 symbols').isLength({
-      min: 5,
-    }),
-  ],
-  controller.register
-);
+router.post('/register', controller.register);
 
 module.exports = router;
