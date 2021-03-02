fe12923e905901bdcdfd0a3affb4b9947e055651
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-03-02 17:50:52 +0500

    Added redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header/Header';
 import { Route, Redirect, BrowserRouter, Switch } from 'react-router-dom';
 import Login from './components/authorization/Login';
 import Registration from './components/authorization/Registration';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from './actions/user';
 
 function App() {
   const isAuth = useSelector((state) => state.userReducer.isAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(auth());
+  }, []);
+
   return (
     <BrowserRouter>
       <div className='App'>
@@ -15,6 +22,7 @@ function App() {
           <Switch>
             <Route path='/login' component={Login} />
             <Route path='/register' component={Registration} />
+            <Redirect to='/login' />
           </Switch>
         )}
       </div>
