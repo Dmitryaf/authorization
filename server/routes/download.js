391e322292e50a1b36bcce7e99e1065bb1e20ec0
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-03-03 16:22:28 +0500

    Deleted

--- server/routes/download.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/download');
-const upload = require('../middleware/upload');
-
-router.post('/', upload.single('file'), controller.create);
-
-module.exports = router;
