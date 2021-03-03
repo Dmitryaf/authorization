e22aeb3e609b3c613fe744d4f37fbcc49073edd5
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-03-03 17:37:20 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Header from './components/Header/Header';
 import { Route, Redirect, BrowserRouter, Switch } from 'react-router-dom';
+import Disk from './components/Disk/Disk';
 import Login from './components/authorization/Login';
 import Registration from './components/authorization/Registration';
 import { useDispatch, useSelector } from 'react-redux';
@@ -18,11 +19,16 @@ function App() {
     <BrowserRouter>
       <div className='App'>
         <Header />
-        {!isAuth && (
+        {!isAuth ? (
           <Switch>
             <Route path='/login' component={Login} />
             <Route path='/register' component={Registration} />
             <Redirect to='/login' />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path='/' component={Disk} />
+            <Redirect to='/' />
           </Switch>
         )}
       </div>
