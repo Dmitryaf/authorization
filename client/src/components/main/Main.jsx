2d80a22e3831d0704e17ea9d630138ba9fbffe2d
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-29 12:42:19 +0500

    Added main component

---
+++ client/src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Main.module.scss';
+
+function Main() {
+  return (
+    <div>
+      <h1 className={styles.title}>Вы вывошли!</h1>
+    </div>
+  );
+}
+
+export default Main;
